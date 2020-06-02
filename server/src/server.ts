7ce2bd3095b0c9298c666392f8ca5b32e9bfb05f
LuizAlexandreSilva <alexandrehsilva96@gmail.com>
2020-06-02 18:22:14 -0400

    second lesson

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response) => {
-  response.json({ message: 'Hello World' });
-});
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
